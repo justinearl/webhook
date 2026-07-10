dad22fa29b4dde745d6c8eb49bb6c8a2abe36e2b
justinearl <j.guevarra@psp.team>
2026-07-10 11:02:04 +0800

    add: remove CORS

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from contextlib import asynccontextmanager
 
 import logfire
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from api.config import LOGFIRE_TOKEN
 from api.db import Base, engine
@@ -18,14 +17,6 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 if LOGFIRE_TOKEN is not None:
     logfire.configure(token=LOGFIRE_TOKEN)
